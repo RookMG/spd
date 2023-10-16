d4c96c041f20655a830fc8e8e98b2bf893ad8a90
김민겸 <lqaks456@gmail.com>
2023-10-16 12:01:08 +0900

    Docs: C# async 테스크 취소 공부

--- study/김민겸/C#/Project1/Project1/test.cs
+++ study/김민겸/C#/Project1/Project1/test.cs
@@ -1,51 +1,102 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        ProcessMultipleWriteAsync().Wait();
+        Console.WriteLine("Application started.");
+        Console.WriteLine("Press the ENTER key to cancel...\n");
+
+        Task cancelTask = Task.Run(() =>
+        {
+            while (Console.ReadKey().Key != ConsoleKey.Enter)
+            {
+                Console.WriteLine("Press the ENTER key to cancel...");
+            }
+            Console.WriteLine("\nENTER key pressed: cancelling downloads.\n");
+            s_cts.Cancel();
+        });
+
+        Task sumPageSizesTask = SumPageSizesAsync();
+
+        Task finishedTask = await Task.WhenAny(new[] { cancelTask, sumPageSizesTask });
+        if(finishedTask == cancelTask)
+        {
+            try
+            {
+                await sumPageSizesTask;
+                Console.WriteLine("Download task completed before cancel request was processed.");
+            }
+            catch(TaskCanceledException)
+            {
+                Console.WriteLine("Download task has been cancelled.");
+            }
+        }
+        Console.WriteLine("Application ending");
     }
 
-    static async Task ProcessMultipleWriteAsync()
+    static readonly CancellationTokenSource s_cts = new CancellationTokenSource();
+    static readonly HttpClient s_client = new HttpClient
+    { 
+        MaxResponseContentBufferSize = 1_000_000 
+    };
+    static readonly IEnumerable<string> s_urlList = new string[]
     {
-        IList<FileStream> sourceStreams = new List<FileStream>();
+        "https://learn.microsoft.com",
+        "https://learn.microsoft.com/aspnet/core",
+        "https://learn.microsoft.com/azure",
+        "https://learn.microsoft.com/azure/devops",
+        "https://learn.microsoft.com/dotnet",
+        "https://learn.microsoft.com/dynamics365",
+        "https://learn.microsoft.com/education",
+        "https://learn.microsoft.com/enterprise-mobility-security",
+        "https://learn.microsoft.com/gaming",
+        "https://learn.microsoft.com/graph",
+        "https://learn.microsoft.com/microsoft-365",
+        "https://learn.microsoft.com/office",
+        "https://learn.microsoft.com/powershell",
+        "https://learn.microsoft.com/sql",
+        "https://learn.microsoft.com/surface",
+        "https://learn.microsoft.com/system-center",
+        "https://learn.microsoft.com/visualstudio",
+        "https://learn.microsoft.com/windows",
+        "https://learn.microsoft.com/xamarin"
+    };
 
-        try
+    static async Task SumPageSizesAsync()
+    {
+        var stopwatch = Stopwatch.StartNew();
+
+        var taskList = new List<Task<int>>();
+        foreach(string url in s_urlList)
         {
-            string folder = Directory.CreateDirectory("tempfolder").Name;
-            IList<Task> writeTaskList = new List<Task>();
-
-            for(int index = 1; index <= 10; ++index)
-            {
-                string fileName = $"file-{index:00}.txt";
-                string filePath = $"{folder}/{fileName}";
-                string text = $"In file {index}{Environment.NewLine}";
-                byte[] encodedText = Encoding.Unicode.GetBytes(text);
-
-                var sourceStream = new FileStream(
-                    filePath,
-                    FileMode.Create, FileAccess.Write, FileShare.None,
-                    bufferSize: 4096, useAsync: true);
-
-                Task writeTask = sourceStream.WriteAsync(encodedText, 0, encodedText.Length);
-                sourceStreams.Add(sourceStream);
-                writeTaskList.Add(writeTask);
-            }
-            await Task.WhenAll(writeTaskList);
+            taskList.Add(ProcessUrlAsync(url, s_client, s_cts.Token));
         }
-        finally
+        int[] results = await Task.WhenAll(taskList);
+
+        int total = 0;
+        foreach (int contentLength in results)
         {
-            foreach(var sourceStream in sourceStreams)
-            {
-                sourceStream.Close();
-            }
+            total += contentLength;
         }
+
+        stopwatch.Stop();
+
+        Console.WriteLine($"\nTotal bytes returned: {total:#,#}");
+        Console.WriteLine($"Elapsed time:\t{stopwatch.Elapsed}\n");
+    }
+
+    static async Task<int> ProcessUrlAsync(string url, HttpClient client, CancellationToken token)
+    {
+        HttpResponseMessage response = await client.GetAsync(url, token);
+        byte[] content = await response.Content.ReadAsByteArrayAsync();
+        Console.WriteLine($"{url,-60} {content.Length,10:#,#}");
+        return content.Length;
     }
 }
 
