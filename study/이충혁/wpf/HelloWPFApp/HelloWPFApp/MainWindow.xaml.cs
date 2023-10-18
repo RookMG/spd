9c1d351575fa9b9fca6c8ae85418d6814bb6d58d
unknown <chyuk98@naver.com>
2023-10-18 18:03:31 +0900

    Chore: Study wpf dialog

--- study/이충혁/wpf/HelloWPFApp/HelloWPFApp/MainWindow.xaml.cs
+++ study/이충혁/wpf/HelloWPFApp/HelloWPFApp/MainWindow.xaml.cs
@@ -20,56 +20,9 @@ namespace HelloWPFApp
     /// </summary>
     public partial class MainWindow : Window
     {
-
-        private IDWMouseOperation test;
-
-        public interface IDWMouseOperation
-        {
-            /// <summary>
-
-            /// </summary>
-            void DWMouseDown(object sender, MouseButtonEventArgs e);
-            /// <summary>
-
-            /// </summary>
-            void DWMouseMove(object sender, MouseEventArgs e);
-            /// <summary>
-
-            /// </summary>
-            void DWMouseUp(object sender, MouseButtonEventArgs e);
-        }
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        private void NewDocument(object sender, RoutedEventArgs e)
-        {
-
-            MessageBox.Show("점", "test");
-        }
-
-        private void Button_Click_Dot(object sender, RoutedEventArgs e)
-        {
-            
-            MessageBox.Show("점", "test");
-        }
-
-        private void Button_Click_Line(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("선", "test");
-        }
-
-        private void Button_Click_Rectangle(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("사각", "test");
-        }
-
-        private void Button_Click_Polygon(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("폴리", "test");
-        }
-
-      
     }
 }
---
+++ study/이충혁/wpf/HelloWPFApp/HelloWPFApp/ToolBar.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using netDxf;
+using netDxf.Entities;
+using Microsoft.Win32;
+
+namespace HelloWPFApp
+{
+    /// <summary>
+    /// ToolBar.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ToolBar : Page
+    {
+        public ToolBar()
+        {
+            InitializeComponent();
+        }
+
+        private void NewDocumentButton(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("새 문서");
+
+            //string file = "abcd.dxf";
+            //DxfDocument doc = new DxfDocument();
+            //var entity = new netDxf.Entities.Line(new Vector2(5, 5), new Vector2(10, 5));
+            //doc.Entities.Add(entity);
+            //doc.Save(file);
+
+        }
+
+        private void LoadDocumentButton(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "dxf 파일 (*.dxf)|*.dxf";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+            }
+        }
+
+        private void SaveDocumentButton(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("새 문서");
+
+            //string file = "abcd.dxf";
+            //DxfDocument doc = new DxfDocument();
+            //var entity = new netDxf.Entities.Line(new Vector2(5, 5), new Vector2(10, 5));
+            //doc.Entities.Add(entity);
+            //doc.Save(file);
+
+        }
+    }
+}
