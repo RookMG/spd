1f3994e9280e762976a220ac8df6664d39bafaee
chyuk98@naver.com <chyuk98@naver.com>
2023-10-24 16:57:22 +0900

    Chore: study tree view

---
+++ study/이충혁/wpf/dxfEditorPrototype/dxfEditorPrototype/EntityDetails.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dxfEditorPrototype
+{
+    /// <summary>
+    /// EntityDetails.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class EntityDetails : Page
+    {
+        public EntityDetails()
+        {
+            InitializeComponent();
+            Utils.Mediator.Register("MainWindow.ShowEntityTypes", ShowEntityTypes);
+        }
+
+        public void ShowEntityTypes(object obj)
+        {
+            foreach (var line in MainWindow.doc.Entities.Lines)
+            {
+                //var linePlot = WpfPlot1.Plot.AddLine(line.StartPoint.X, line.StartPoint.Y, line.EndPoint.X, line.EndPoint.Y);
+
+
+            }
+
+            foreach (var circle in MainWindow.doc.Entities.Circles)
+            {
+                //WpfPlot1.Plot.AddCircle(x: circle.Center.X, y: circle.Center.Y, radius: circle.Radius);
+            }
+
+            foreach (var ellipse in MainWindow.doc.Entities.Ellipses)
+            {
+                //var ellipsePlot = WpfPlot1.Plot.AddEllipse(x: ellipse.Center.X, y: ellipse.Center.Y, xRadius: ellipse.MajorAxis / 2, yRadius: ellipse.MinorAxis / 2);
+
+            }
+        }
+    }
+}
