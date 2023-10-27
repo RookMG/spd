fdb05dbe5bc34d5591af9da1a92c96ba9e95a924
tw <tackskim@naver.com>
2023-10-27 16:38:47 +0900

    feat: file path로 변경4

--- SEMES_Pixel_Designer/Utils/Mediator.cs
+++ SEMES_Pixel_Designer/Utils/Mediator.cs
@@ -43,7 +43,7 @@ namespace SEMES_Pixel_Designer.Utils
         //호출하기
         static public void NotifyColleagues(string token, object args)
         {
-            MessageBox.Show("Debug : "+token+" 함수 실행");
+            /*MessageBox.Show("Debug : "+token+" 함수 실행");*/
             if (pl_dict.ContainsKey(token))
                 foreach (var callback in pl_dict[token])
                     callback(args);
