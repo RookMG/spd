b37fb4beb1d29d9a38b880dc0cfe3e6b50424f8d
tngks1995 <tngks1995@naver.com>
2023-11-10 15:26:38 +0900

    Chore: 초기 미니맵 크기 수정

--- SEMES_Pixel_Designer/View/Minimap.xaml.cs
+++ SEMES_Pixel_Designer/View/Minimap.xaml.cs
@@ -57,7 +57,6 @@ namespace SEMES_Pixel_Designer.View
 
         public void UpdatePosition()
         {
-            // TODO : 구현
             double sx = (Coordinates.minX - Coordinates.glassLeft) * ActualWidth / (Coordinates.GetGlassRight() - Coordinates.glassLeft),
                 sy = (Coordinates.GetGlassTop() - Coordinates.minY) * ActualHeight / (Coordinates.GetGlassTop() - Coordinates.glassBottom),
                 ex = (Coordinates.maxX - Coordinates.glassLeft) * ActualWidth / (Coordinates.GetGlassRight() - Coordinates.glassLeft),
@@ -70,7 +69,6 @@ namespace SEMES_Pixel_Designer.View
 
         public void AdjustRatio()
         {
-            // TODO : 구현
             if((Coordinates.GetGlassTop() - Coordinates.glassBottom) / (Coordinates.GetGlassRight() - Coordinates.glassLeft) > Window.GetWindow(this).ActualHeight / Window.GetWindow(this).ActualWidth)
             {
                 Height = Window.GetWindow(this).ActualHeight - 40;
