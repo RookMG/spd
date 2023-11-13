4cb4e135d2026bf31062f2a3ea8a4c5d6d43042e
tngks1995 <tngks1995@naver.com>
2023-11-13 11:12:08 +0900

    Chore : 활성 셀 변경

--- SEMES_Pixel_Designer/View/MainDrawer.xaml.cs
+++ SEMES_Pixel_Designer/View/MainDrawer.xaml.cs
@@ -102,7 +102,8 @@ namespace SEMES_Pixel_Designer
             cells.Add(new Cell(100, 100, 372, 372, 1000, 1000));
             cells.Add(new Cell(1000000, 100, 372, 372, 1000, 1000));
             cells.Add(new Cell(100, 1000000, 372, 372, 1000, 1000));
-            selectedCell = cells[3];
+            // cells.Add(new Cell(1000000, 1000000, 372, 372, 1000, 1000));
+            selectedCell = cells[2];
 
             Minimap minimap = new Minimap();
             minimap.Show();
